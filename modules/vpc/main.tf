28e423f8697b4e5f81186dedf9b3d9ce31a43b4f
AyaShosha <ayaalaashoshaa@gmail.com>
2024-09-21 02:08:24 +0300

    main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,55 +1,108 @@
-provider "aws" {
-  region = var.region
-}
-
-resource "aws_vpc" "main" {
+resource "aws_vpc" "my_vpc" {
   cidr_block = var.vpc_cidr
 }
 
-resource "aws_subnet" "public" {
-  count = 2
-  vpc_id = aws_vpc.main.id
-  cidr_block = element(var.public_subnet_cidrs, count.index)
-  availability_zone = element(var.availability_zones, count.index)
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.public_subnet_cidr
+  availability_zone = var.az1
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "private" {
-  count = 2
-  vpc_id = aws_vpc.main.id
-  cidr_block = element(var.private_subnet_cidrs, count.index)
-  availability_zone = element(var.availability_zones, count.index)
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.private_subnet_1_cidr
+  availability_zone = var.az1
 }
 
-resource "aws_security_group" "bastion" {
-  vpc_id = aws_vpc.main.id
-  // Define your security group rules here
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.private_subnet_2_cidr
+  availability_zone = var.az2
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_security_group" "bastion_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.bastion_ip}/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "nginx_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "bastion" {
   ami           = var.bastion_ami
   instance_type = var.bastion_instance_type
-  subnet_id     = aws_subnet.public[0].id
-  security_groups = [aws_security_group.bastion.name]
+  subnet_id     = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.bastion_sg.name]
+
+  provisioner "local-exec" {
+    command = "echo Bastion Host Public IP: ${self.public_ip}"
+  }
 }
 
-resource "aws_instance" "nginx_private_1" {
+resource "aws_instance" "nginx_1" {
   ami           = var.nginx_ami
   instance_type = var.nginx_instance_type
-  subnet_id     = aws_subnet.private[0].id
-  // Security group to allow access from Load Balancer
+  subnet_id     = aws_subnet.private_subnet_1.id
+  security_groups = [aws_security_group.nginx_sg.name]
 }
 
-resource "aws_instance" "nginx_private_2" {
+resource "aws_instance" "nginx_2" {
   ami           = var.nginx_ami
   instance_type = var.nginx_instance_type
-  subnet_id     = aws_subnet.private[1].id
-  // Security group to allow access from Load Balancer
+  subnet_id     = aws_subnet.private_subnet_2.id
+  security_groups = [aws_security_group.nginx_sg.name]
 }
 
 resource "aws_elb" "nginx_lb" {
-  name               = "nginx-lb"
-  availability_zones = var.availability_zones
+  name               = "nginx-load-balancer"
+  availability_zones = [var.az1, var.az2]
 
   listener {
     instance_port     = 80
@@ -66,8 +119,5 @@ resource "aws_elb" "nginx_lb" {
     unhealthy_threshold = 2
   }
 
-  instances = [
-    aws_instance.nginx_private_1.id,
-    aws_instance.nginx_private_2.id,
-  ]
+  instances = [aws_instance.nginx_1.id, aws_instance.nginx_2.id]
 }
