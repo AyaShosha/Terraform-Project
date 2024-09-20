b9f7ce482cddc2541dfe37b137c8a0e5cb7ba6e6
AyaShosha <ayaalaashoshaa@gmail.com>
2024-09-21 02:09:35 +0300

    variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,15 +1,11 @@
-variable "region" {}
 variable "vpc_cidr" {}
-variable "public_subnet_cidrs" {
-  type = list(string)
-}
-variable "private_subnet_cidrs" {
-  type = list(string)
-}
-variable "availability_zones" {
-  type = list(string)
-}
+variable "public_subnet_cidr" {}
+variable "private_subnet_1_cidr" {}
+variable "private_subnet_2_cidr" {}
+variable "az1" {}
+variable "az2" {}
+variable "bastion_ip" {}
 variable "bastion_ami" {}
-variable "bastion_instance_type" {}
 variable "nginx_ami" {}
+variable "bastion_instance_type" {}
 variable "nginx_instance_type" {}
