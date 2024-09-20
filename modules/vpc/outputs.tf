c2ee1249c30f478ead572b4e885c63252b854a4d
AyaShosha <ayaalaashoshaa@gmail.com>
2024-09-21 02:09:59 +0300

    outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,3 +1,7 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
+output "bastion_ip" {
+  value = aws_instance.bastion.public_ip
+}
+
+output "load_balancer_dns" {
+  value = aws_elb.nginx_lb.dns_name
 }
